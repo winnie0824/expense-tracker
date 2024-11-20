a4f399e0b49d15bc16c1e56f3fd0030a6aa4f8de
winnie0824 <a0953776565@gmail.com>
2024-11-20 15:10:00 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        stream: false,
+        crypto: false,
+        fs: false,
+        path: false,
+      };
+    }
+    return config;
+  },
 }
+
+module.exports = nextConfig
