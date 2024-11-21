9e6240bae7a2f17a87d74d78a92a7acf0df37ee1
winnie0824 <a0953776565@gmail.com>
2024-11-21 17:35:54 +0800

    Create xlsx.d.ts

---
+++ types/xlsx.d.ts
@@ -0,0 +1,13 @@
+declare module 'xlsx' {
+  export interface WorkBook {}
+  export interface WorkSheet {}
+  
+  export interface Utils {
+    book_new(): WorkBook;
+    json_to_sheet<T>(data: T[], opts?: any): WorkSheet;
+    book_append_sheet(wb: WorkBook, ws: WorkSheet, name: string): void;
+  }
+  
+  export const utils: Utils;
+  export function writeFile(wb: WorkBook, filename: string): void;
+}
