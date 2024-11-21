b94db272015e19387a56fe3eba7620039958b0db
winnie0824 <a0953776565@gmail.com>
2024-11-21 15:24:05 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
       fs: false,
       stream: false,
       crypto: false,
-      path: false,
       process: false,
+      path: false,
       buffer: false,
-      util: false,
-      assert: false,
-      worker_threads: false,
-      os: false,
-      readline: false,
     };
     return config;
   },
-  transpilePackages: ['xlsx'],
 }
 
 module.exports = nextConfig
